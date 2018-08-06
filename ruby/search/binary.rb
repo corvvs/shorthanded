caf090b22fa8ea0544dbe26f776f1461cc7761e4
河田鷹介 <yosuke@stily.co.jp>
2018-08-06 11:26:38 +0900

    add: binary search for float

--- ruby/search/binary.rb
+++ ruby/search/binary.rb
@@ -1,18 +1,36 @@
 
-# find an index: max{ i | yield(i, array) == true  }
-def bsearch_lefting(array)
-  return -1 if !yield(0, array)
-  a,b = 0,array.size-1
+# find an index: max{ i | yield(i) == true  }
+def bsearch_int_left(a, b)
+  return nil if !yield(a)
   while a < b
     m = a + (b-a+1)/2
-    if yield(m, array)
+    if yield(m)
       a = m
     else
       b = m-1
     end
   end
-  a <= b ? a : -1
+  a <= b ? a : nil
+end
+
+# find a number x near to: max{ x | yield(x) == true }
+def bsearch_float_left(a, b, delta)
+  return nil if !yield(a)
+  while b - a >= delta
+    m = a + (b-a)/2
+    if yield(m)
+      a = m
+    else
+      b = m
+    end
+  end
+  a + (b-a)/2
 end
 
 # TESTCODE
-puts bsearch_lefting((1..100000).to_a) { |i,arr| arr[i] <= 70000 }
+
+#arr = (1..100000).to_a
+#puts bsearch_int_left(0, arr.size-1) { |i| arr[i] <= 70000 }
+
+delta = 1E-12
+puts bsearch_float_left(0.0, 2.0, delta) { |x| Math.cos(x) >= 0 } * 2 - Math::PI
